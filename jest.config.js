48cfc71a91e16ac9e0f7ea1bf76735a0a2dd8bd1
Samuel Gomes <samuel.gomes@onerf.com.br>
2023-08-22 16:13:08 -0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: "ts-jest/presets/js-with-ts",
+    testEnvironment: "node",
+    coveragePathIgnorePatterns: ["/node_modules/", "/src/", "dist"],
+    rootDir: "./__tests__/app",
+    verbose: true,
+    modulePathIgnorePatterns: [".docker", "node_modules", "./__tests__/app/coverage", "./__tests__/app/__mocks__", "src", "dist", "css_dependecy"],
+    transform: {
+        "node_modules/variables/.+\\.(j|t)sx?$": "ts-jest",
+    },
+    transformIgnorePatterns: ["node_modules/(?!variables/.*)"],
+    coverageDirectory: "coverage",
+    clearMocks: true,
+    collectCoverage: false,
+};
